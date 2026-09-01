f4a4a138410f50d4da05b96c1807c7dccfed80fd
Salman Khan <khamsalman73@gmail.com>
2026-09-01 10:09:31 +0530

    udtaing 704

--- Day5/ClassWork/704.java
+++ Day5/ClassWork/704.java
@@ -8,7 +8,9 @@ class Solution {
 
             if (nums[mid] == target) {
                 return mid;
-            } else if (nums[mid] < target) {
+            }
+
+            if (nums[mid] < target) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
